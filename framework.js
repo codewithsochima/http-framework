23b034f0c86cd9044a722b68af44953a93e9f25e
Sochima <humpheryprecious07@gmail.com>
2026-08-14 13:00:04 +0100

    initial HTTP framework

---
+++ framework.js
@@ -0,0 +1,76 @@
+const http = require("http");
+const routes = [];
+
+function framework() {
+  const server = http.createServer((req, res) => {
+    let body = "";
+
+    const route = routes.find((route) => {
+      return route.method === req.method && route.path === req.url;
+    });
+
+    console.log(route);
+
+    if (!route) {
+      res.statusCode = 404;
+      return res.end("Route not found");
+    }
+
+    req.on("data", (chunk) => {
+      body += chunk.toString();
+    });
+
+    req.on("end", () => {
+      if (body) {
+        try {
+          req.body = JSON.parse(body);
+        } catch (error) {
+          res.statusCode = 400;
+          return res.end("Invalid JSON");
+        }
+      }
+
+      route.handler(req, res);
+    });
+  });
+
+  return {
+    get: function (path, handler) {
+      routes.push({
+        method: "GET",
+        path: path,
+        handler: handler,
+      });
+    },
+
+    post: function (path, handler) {
+      routes.push({
+        method: "POST",
+        path: path,
+        handler: handler,
+      });
+    },
+
+    put: function (path, handler) {
+      routes.push({
+        method: "PUT",
+        path: path,
+        handler: handler,
+      });
+    },
+
+    delete: function (path, handler) {
+      routes.push({
+        method: "DELETE",
+        path: path,
+        handler: handler,
+      });
+    },
+
+    listen: function (port) {
+      server.listen(port);
+    },
+  };
+}
+
+module.exports = framework;
---
+++ server.js
@@ -0,0 +1,18 @@
+const framework = require("./framework");
+const app = framework();
+
+app.get("/items", (req, res) => {
+  res.end("Here are the items");
+});
+app.post("/items", (req, res) => {
+  res.end("Item created");
+});
+app.put("/items", (req, res) => {
+  res.end("Item updated");
+});
+app.delete("/items", (req, res) => {
+  res.end("Item deleted");
+});
+
+app.listen(3000);
+
